9d300f43939ba633299d829466b15fb555eab132
LGZUYD <2305329goossens@zuyd.nl>
2024-10-17 10:24:20 +0200

    Fix

--- Models/Post.cs
+++ Models/Post.cs
@@ -1,14 +1,20 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Casusvictuz
 {
     public class Post
     {
+        
         public int Id { get; set; }
-        public string? Content { get; set; } 
+        [Required]
+        public required string Content { get; set; }
+        [Required]
         public DateTime Date { get; set; }
-        public int UserId { get; set; }
+        [Required]
+        public int UserId { get; set; }        
         public virtual required User User { get; set; }
+        [Required]
         public int CategoryId { get; set; }
         public virtual required Category Category { get; set; }
     }
