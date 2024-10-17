33bc2324b45f631fdb68dc63c76c73bad26e8d0d
LGZUYD <2305329goossens@zuyd.nl>
2024-10-17 10:17:33 +0200

    Forum class opzet

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -1,9 +1,15 @@
 using System;
+using System.Reflection.Metadata;
 
 namespace Casusvictuz
 {
     public class Comment : Post
     {
-        public int PostId { get; set; }
+        public int ThreadId { get; set; }
+        public Thread Thread { get; set; }
+        public int? ParentCommentId { get; set; }
+        public virtual Comment ParentComment { get; set; }
+        public ICollection<Comment>? Replies { get; set; }
+
     }
 }
