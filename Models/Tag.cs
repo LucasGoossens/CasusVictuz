7aa21bc67cd32f0dd8488b5890034120019d1584
FloydGitHub <2204860kersemakers@zuyd.nl>
2024-10-18 09:30:42 +0200

    Protential fix db

--- Models/Tag.cs
+++ Models/Tag.cs
@@ -4,6 +4,7 @@ namespace CasusVictuz.Models
 {
     public class Tag
     {
+        [Key]
         public int Id { get; set; }
         [Required]
         public required string Name { get; set; }
