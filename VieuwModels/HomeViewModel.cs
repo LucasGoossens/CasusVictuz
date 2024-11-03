7f1e09f2554b45c217c5ba30db26d8ba179356a7
FloydGitHub <2204860kersemakers@zuyd.nl>
2024-11-03 18:34:23 +0100

    Homepage + kleine layout aanpassing

---
+++ VieuwModels/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Casusvictuz;
+using CasusVictuz.Models;
+
+namespace CasusVictuz.VieuwModels
+{
+    public class HomeViewModel
+    {
+        public Event? NextEvent { get; set; }
+        public List<Event> Events { get; set; } = new List<Event>();
+    }
+}
